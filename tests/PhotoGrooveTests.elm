0da6cc6fa641d007c7524ceef5dafa20a3def373
Jonathan Avinor <javinor@gmail.com>
2021-01-29 11:31:06 +0200

    ch 6.2.2 - fuzz tests

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,32 @@
+module PhotoGrooveTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
+import PhotoGroove exposing (Model, Msg(..), Photo, initialModel, update)
+import Test exposing (..)
+
+
+decoderTest : Test
+decoderTest =
+    fuzz2 string int "title defaults to\n(untitled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
+
+
+slidHueSetsHue : Test
+slidHueSetsHue =
+    fuzz int "SlidHue sets the hue" <|
+        \amount ->
+            initialModel
+                |> update (SlidHue amount)
+                |> Tuple.first
+                |> .hue
+                |> Expect.equal amount
